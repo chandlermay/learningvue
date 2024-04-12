53a7f413181f433f103bdd1f137a9d69ad8ed214
chandlermay <133597587+chandlermay@users.noreply.github.com>
2024-04-11 23:21:21 -0400

    moved resume to views. trying to figure out why the nav is in the middleof screen

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -16,7 +16,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/resume',
+      name: 'resume',
+      component: () => import('../views/Resume.vue'),
+    },
+/*     {
+      path: '/',
+      name: '',
+      component: '',
+    }, */
   ]
 })
 
